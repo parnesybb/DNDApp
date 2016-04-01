d40c428f71b872313fbcf04609b02113b14e2064
Justin Cather <jdc1014@sru.edu>
2016-03-31 20:50:56 -0400

    Init

---
+++ GoSteve/Screens/CharacterSheetScreen.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace GoSteve
+{
+    [Activity(Label = "GoSteve! Dungeons and Dragons", MainLauncher = true, Icon = "@drawable/icon")]
+    public class CharacterSheetScreen : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.CharacterSheet);
+
+            // Set fields.
+            Button classSelectButton = FindViewById<Button>(Resource.Id.classSelect);
+            Button backgroundSelectButton = FindViewById<Button>(Resource.Id.charBG);
+            Button raceSelectButton = FindViewById<Button>(Resource.Id.race);
+            Button subRaceSelectButton = FindViewById<Button>(Resource.Id.subRace);
+            Button alignmentSelectButton = FindViewById<Button>(Resource.Id.alignment);
+
+            // Class selection.
+            classSelectButton.Click += (s, arg) => 
+            {
+                PopupMenu menu = new PopupMenu(this, classSelectButton);
+                menu.Inflate(Resource.Xml.ClassList);
+                menu.Show();
+
+                menu.MenuItemClick += (s1, arg1) => 
+                {
+                    classSelectButton.Text = arg1.Item.TitleFormatted.ToString();
+                };
+            };
+
+            // Background selection.          
+            backgroundSelectButton.Click += (s, arg) =>
+            {
+                PopupMenu m = new PopupMenu(this, backgroundSelectButton);
+                m.Inflate(Resource.Xml.BackgroundList);
+                m.Show();
+
+                m.MenuItemClick += (s1, arg1) =>
+                {
+                    backgroundSelectButton.Text = arg1.Item.TitleFormatted.ToString();
+                };
+            };
+
+            // Race selection.           
+            raceSelectButton.Click += (s, arg) =>
+            {               
+                PopupMenu m = new PopupMenu(this, raceSelectButton);
+                m.Inflate(Resource.Xml.RaceList);
+                m.Show();
+
+                m.MenuItemClick += (s1, arg1) =>
+                {
+                    var prev = raceSelectButton.Text;
+                    raceSelectButton.Text = arg1.Item.TitleFormatted.ToString();
+
+                    if (prev != raceSelectButton.Text)
+                    {
+                        subRaceSelectButton.Text = "None";
+                    }
+                };
+            };
+
+            // Sub-race selection.      
+            subRaceSelectButton.Click += (s, arg) =>
+            {
+                PopupMenu m = new PopupMenu(this, subRaceSelectButton);
+
+                switch (raceSelectButton.Text)
+                {
+                    case KnownValues.Races.DWARF:
+                        m.Inflate(Resource.Xml.dwarfSub);
+                        break;
+
+                    case KnownValues.Races.ELF:
+                        m.Inflate(Resource.Xml.elfSub);
+                        break;
+
+                    case KnownValues.Races.GNOME:
+                        m.Inflate(Resource.Xml.gnomeSub);
+                        break;
+
+                    case KnownValues.Races.HALFLING:
+                        m.Inflate(Resource.Xml.halflingSub);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                m.Show();
+
+                m.MenuItemClick += (s1, arg1) =>
+                {
+                    subRaceSelectButton.Text = arg1.Item.TitleFormatted.ToString();
+                };
+            };
+
+            // Alignment selection.
+            alignmentSelectButton.Click += (s, arg) =>
+            {
+                PopupMenu m = new PopupMenu(this, alignmentSelectButton);
+                m.Inflate(Resource.Xml.alignment);
+                m.Show();
+
+                m.MenuItemClick += (s1, arg1) =>
+                {
+                    alignmentSelectButton.Text = arg1.Item.TitleFormatted.ToString();
+                };
+            };
+
+        }
+    }
+}
+
---
+++ GoSteve/Structures/KnownValues.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GoSteve
+{
+    public static class KnownValues
+    {
+        public static class Races
+        {
+            public const string DWARF = "Dwarf";
+            public const string ELF = "Elf";
+            public const string HALFLING = "Halfling";
+            public const string HUMAN = "Human";
+            public const string DRAGONBORN = "Dragonborn";
+            public const string GNOME = "Gnome";
+            public const string HALF_ELF = "Half-Elf";
+            public const string HALF_ORC = "Half-Orc";
+            public const string TIEFLING = "Tiefling";
+        }
+    }
+}
