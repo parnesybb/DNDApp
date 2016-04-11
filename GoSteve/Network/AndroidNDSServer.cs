177be4ecac6518ce4b59080905de9274dd1ebfff
BrianAtwell <brianatwell001@gmail.com>
2016-04-11 12:01:50 -0400

    Network Framework Basic

---
+++ GoSteve/Network/AndroidNDSServer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net.Nsd;
+
+namespace GoSteve.Network
+{
+    class AndroidNDSServer
+    {
+        public void startServer()
+        {
+
+        }
+
+        public void stopServer()
+        {
+
+        }
+
+        public void registerService()
+        {
+            NsdServiceInfo serviceInfo = new NsdServiceInfo();
+        }
+    }
+}
---
+++ GoSteve/Network/DMServer.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GoSteve.Network
+{
+    class DMServer
+    {
+        public void startIPServer()
+        {
+
+        }
+
+        public void stopIPServer()
+        {
+
+        }
+
+        public void startDNSServer()
+        {
+
+        }
+
+        public void stopDNSServer()
+        {
+
+        }
+
+        public void startServer()
+        {
+
+        }
+
+        public void stopServer()
+        {
+        }
+
+        public void translateXMLToObject()
+        {
+
+        }
+    }
+}
