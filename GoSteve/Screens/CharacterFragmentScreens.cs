5c926141dc28bead72bea91f61f23139bbd262bc
Justin Cather <jdc1014@sru.edu>
2016-04-28 23:35:46 -0400

    Saving throws and skills

--- GoSteve/Screens/CharacterFragmentScreens.cs
+++ GoSteve/Screens/CharacterFragmentScreens.cs
@@ -27,6 +27,7 @@ namespace GoSteve.Screens
         {
             _view = inflater.Inflate(Resource.Layout.CharacterStatsSkillsScreen, null);
 
+            // BASE STATS
             var input = _view.FindViewById<EditText>(Resource.Id.StatsSkillsStrength);
             input.Text = _cs.AbilitiesAndStats.Strength.ToString();
             input.TextChanged += Input_TextChanged;
@@ -51,12 +52,41 @@ namespace GoSteve.Screens
             input.Text = _cs.AbilitiesAndStats.Charisma.ToString();
             input.TextChanged += Input_TextChanged;
 
+            // MODS
             _view.FindViewById<TextView>(Resource.Id.StatsSkillsStrengthMod).Text = _cs.AbilitiesAndStats.StrengthMod.ToString();
             _view.FindViewById<TextView>(Resource.Id.StatsSkillsDexterityMod).Text = _cs.AbilitiesAndStats.DexMod.ToString();
             _view.FindViewById<TextView>(Resource.Id.StatsSkillsConMod).Text = _cs.AbilitiesAndStats.ConstMod.ToString();
             _view.FindViewById<TextView>(Resource.Id.StatsSkillsIntMod).Text = _cs.AbilitiesAndStats.IntelMod.ToString();
             _view.FindViewById<TextView>(Resource.Id.StatsSkillsWisMod).Text = _cs.AbilitiesAndStats.WisdomMod.ToString();
             _view.FindViewById<TextView>(Resource.Id.StatsSkillsCharMod).Text = _cs.AbilitiesAndStats.CharismaMod.ToString();
+
+            // SAVING THROWS
+            _view.FindViewById<TextView>(Resource.Id.StatsSkills_s_saving).Text = _cs.AbilitiesAndStats.StrengthSavingThrow.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkills_d_saving).Text = _cs.AbilitiesAndStats.DexteritySavingThrow.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkills_con_saving).Text = _cs.AbilitiesAndStats.ConstitutionSavingThrow.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkills_i_saving).Text = _cs.AbilitiesAndStats.IntelligenceSavingThrow.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkills_w_saving).Text = _cs.AbilitiesAndStats.WisdomSavingThrow.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkills_char_saving).Text = _cs.AbilitiesAndStats.CharismaSavingThrow.ToString();
+
+            // SKILLS
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsAcrobatics).Text = _cs.AbilitiesAndStats.Acrobatics.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsAnimalHdnl).Text = _cs.AbilitiesAndStats.AnimalHandling.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsArcana).Text = _cs.AbilitiesAndStats.Arcana.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsAthletics).Text = _cs.AbilitiesAndStats.Athletics.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsDeception).Text = _cs.AbilitiesAndStats.Deception.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsHistory).Text = _cs.AbilitiesAndStats.History.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsInsight).Text = _cs.AbilitiesAndStats.Insight.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsIntimidation).Text = _cs.AbilitiesAndStats.Intimidation.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsInvestigation).Text = _cs.AbilitiesAndStats.Investigation.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsMedicine).Text = _cs.AbilitiesAndStats.Medicine.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsNature).Text = _cs.AbilitiesAndStats.Nature.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsPerception).Text = _cs.AbilitiesAndStats.Perception.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsPerformance).Text = _cs.AbilitiesAndStats.Performance.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsPersuasion).Text = _cs.AbilitiesAndStats.Persuasion.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsReligion).Text = _cs.AbilitiesAndStats.Religion.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsSlghtHnd).Text = _cs.AbilitiesAndStats.SlightOfHand.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsStealth).Text = _cs.AbilitiesAndStats.Stealth.ToString();
+            _view.FindViewById<TextView>(Resource.Id.StatsSkillsSurvival).Text = _cs.AbilitiesAndStats.Survival.ToString();
 
             return _view;
         }
@@ -72,26 +102,50 @@ namespace GoSteve.Screens
                     case Resource.Id.StatsSkillsStrength:
                         _cs.AbilitiesAndStats.Strength = Int32.Parse(s.Text);
                         _view.FindViewById<TextView>(Resource.Id.StatsSkillsStrengthMod).Text = _cs.AbilitiesAndStats.StrengthMod.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkills_s_saving).Text = _cs.AbilitiesAndStats.StrengthSavingThrow.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsAthletics).Text = _cs.AbilitiesAndStats.Athletics.ToString();
                         break;
                     case Resource.Id.StatsSkillsDexterity:
                         _cs.AbilitiesAndStats.Dex = Int32.Parse(s.Text);
                         _view.FindViewById<TextView>(Resource.Id.StatsSkillsDexterityMod).Text = _cs.AbilitiesAndStats.DexMod.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkills_d_saving).Text = _cs.AbilitiesAndStats.DexteritySavingThrow.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsAcrobatics).Text = _cs.AbilitiesAndStats.Acrobatics.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsSlghtHnd).Text = _cs.AbilitiesAndStats.SlightOfHand.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsStealth).Text = _cs.AbilitiesAndStats.Stealth.ToString();
                         break;
                     case Resource.Id.StatsSkillsCon:
                         _cs.AbilitiesAndStats.Con = Int32.Parse(s.Text);
                         _view.FindViewById<TextView>(Resource.Id.StatsSkillsConMod).Text = _cs.AbilitiesAndStats.ConstMod.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkills_con_saving).Text = _cs.AbilitiesAndStats.ConstitutionSavingThrow.ToString();
                         break;
                     case Resource.Id.StatsSkillsInt:
                         _cs.AbilitiesAndStats.Intel = Int32.Parse(s.Text);
                         _view.FindViewById<TextView>(Resource.Id.StatsSkillsIntMod).Text = _cs.AbilitiesAndStats.IntelMod.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkills_i_saving).Text = _cs.AbilitiesAndStats.IntelligenceSavingThrow.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsArcana).Text = _cs.AbilitiesAndStats.Arcana.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsHistory).Text = _cs.AbilitiesAndStats.History.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsInvestigation).Text = _cs.AbilitiesAndStats.Investigation.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsNature).Text = _cs.AbilitiesAndStats.Nature.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsReligion).Text = _cs.AbilitiesAndStats.Religion.ToString();
                         break;
                     case Resource.Id.StatsSkillsWis:
                         _cs.AbilitiesAndStats.Strength = Int32.Parse(s.Text);
                         _view.FindViewById<TextView>(Resource.Id.StatsSkillsWisMod).Text = _cs.AbilitiesAndStats.WisdomMod.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkills_w_saving).Text = _cs.AbilitiesAndStats.WisdomSavingThrow.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsAnimalHdnl).Text = _cs.AbilitiesAndStats.AnimalHandling.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsInsight).Text = _cs.AbilitiesAndStats.Insight.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsMedicine).Text = _cs.AbilitiesAndStats.Medicine.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsPerception).Text = _cs.AbilitiesAndStats.Perception.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsSurvival).Text = _cs.AbilitiesAndStats.Survival.ToString();
                         break;
                     case Resource.Id.StatsSkillsChar:
                         _cs.AbilitiesAndStats.Strength = Int32.Parse(s.Text);
                         _view.FindViewById<TextView>(Resource.Id.StatsSkillsCharMod).Text = _cs.AbilitiesAndStats.CharismaMod.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkills_char_saving).Text = _cs.AbilitiesAndStats.CharismaSavingThrow.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsDeception).Text = _cs.AbilitiesAndStats.Deception.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsIntimidation).Text = _cs.AbilitiesAndStats.Intimidation.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsPerformance).Text = _cs.AbilitiesAndStats.Performance.ToString();
+                        _view.FindViewById<TextView>(Resource.Id.StatsSkillsPersuasion).Text = _cs.AbilitiesAndStats.Persuasion.ToString();
                         break;
 
                     default:
