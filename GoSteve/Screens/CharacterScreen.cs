edc50925e2287eb4614eb235b16df1d2d81789fc
Justin Cather <jdc1014@sru.edu>
2016-04-28 20:38:43 -0400

    Started character screen

---
+++ GoSteve/Screens/CharacterScreen.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+namespace GoSteve.Screens
+{
+    [Activity(Label = "Character Screen")]
+    public class CharacterScreen : Activity
+    {
+        private static readonly string TAG = "CharacterScreen";
+        private readonly string[] _tabNames = { "Stats/Skills", "Attributes",  "Prof/Langs", "Equip", "Info"};
+        private CharacterSheet _cs;
+        private int _prevTabPos;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.CharacterBaseScreen);
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+            ActionBar.SetHomeButtonEnabled(true);
+
+            CreateTabs();
+            RecieveMessage();
+        }
+
+        private void RecieveMessage()
+        {
+            var gsMsg = new GSActivityMessage();
+            gsMsg.Message = (byte[])Intent.Extras.Get(gsMsg.CharacterMessage);
+            _cs = CharacterSheet.GetCharacterSheet(gsMsg.Message);
+        }
+
+        private void CreateTabs()
+        {
+            for (int i = 0; i < _tabNames.Length; i++)
+            {
+                var tab = ActionBar.NewTab();
+                tab.SetText(_tabNames[i]);
+                tab.TabSelected += TabClick;
+                ActionBar.AddTab(tab);
+            }
+        }
+
+        private void ClearView()
+        {
+            var viewer = FindViewById<FrameLayout>(Resource.Id.characterScreenDisplay);
+            viewer.RemoveAllViews();
+        }
+
+        private void TabClick(object sender, ActionBar.TabEventArgs e)
+        {
+            var tab = sender as ActionBar.Tab;
+            Log.Debug(TAG, "TabClick -> {0}", tab.Text);
+
+            ClearView();
+
+            switch (tab.Position)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
